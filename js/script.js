21d92178b3cecce0a33289d466c17ddb9a127d5d
Yogeshwari vadivelu <148110568+YOGEVADIVELU@users.noreply.github.com>
2024-01-22 19:26:39 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function descoun(){
+    var a=document.getElementById("coundrp");
+    if( a.style.display==="block"){
+        a.style.display="none";
+    }
+    else{
+        a.style.display="block";
+    }
+}
+function mobimenu(){
+    var b=document.getElementById("mylinks");
+    if(b.style.display==="block"){
+        b.style.display="none";
+    }
+    else{
+        b.style.display="block";
+    }
+}
+function closemenu(){
+    var c=document.getElementById("mylinks");
+    if(c.style.display==="block"){
+        c.style.display="none";
+    }
+    else{
+        c.style.display="block";
+    }
+}
